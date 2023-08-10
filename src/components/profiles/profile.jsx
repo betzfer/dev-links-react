7493901bb1fbe2e1955013ad2550869c5dc54bb5
betzfer <miguelrodriguesdeoliveirasenai@gamil.com>
2023-08-10 17:13:19 -0300

    Finalização parte 1

---
+++ src/components/profiles/profile.jsx
@@ -0,0 +1,8 @@
+export default function profile() {
+    return(
+        <div className="profile">
+            <img src="" alt="" />
+            <p>@Betzfer</p>
+        </div>
+    )
+}
---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import  "./app.css"
+import Profile from "../components/profiles/profile"
+
+export default function App() {
+    return(
+        <>
+        <Profile></Profile>
+        </>
+    );
+}
