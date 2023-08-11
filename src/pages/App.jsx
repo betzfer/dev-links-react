5f14103372c390969829bead9e8bf7cf7179f96f
betzfer <miguelrodriguesdeoliveirasenai@gamil.com>
2023-08-11 10:20:56 -0300

    desenvolvido profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,23 @@
-import  "./app.css"
-import Profile from "../components/profiles/profile"
+import Profile from "../components/profiles/profile";
+import  "./app.css";
 
-export default function App() {
-    return(
-        <>
-        <Profile></Profile>
-        </>
-    );
-}
+import imgDarkMode from "../img/sus.webp";
+import imgLightMode from "../img/suslight.webp";
+import { useState } from "react";
+
+
+    const App = () => {
+        const [isLightMode,setIsLightMode] = useState(false);
+
+        const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+        const imgAlt = isLightMode ? "imagem no modo claro" : "imagem no modo escuro";
+return(
+    <div>
+        <Profile imgSrc={imgSrc} imgAlt={imgAlt}>
+          @Betzfer
+        </Profile>
+    </div>
+);
+    };
+
+export default App;
